0cb02d10d22d6c4644511c28445a1bfc4e6802ca
palak singhania <palak.singhania_cs.ccv19@gla.ac.in>
2022-08-27 23:58:53 +0530

    solved questions of august 2022 lunchtime

---
+++ src/y2022/august/HeightOfRationals.java
@@ -0,0 +1,18 @@
+/*
+ * Author Name: Palak Singhania
+ * IDE: IntelliJ IDEA Ultimate Edition
+ * Date: 27-08-2022
+ */
+
+package y2022.august;
+
+import java.util.Scanner;
+
+public class HeightOfRationals {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(Math.max(a,b));
+    }
+}
